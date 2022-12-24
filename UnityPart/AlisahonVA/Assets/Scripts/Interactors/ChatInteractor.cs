97d597816e5b230116e52e31e37fb4039860da92
Boriswp <59508655+Boriswp@users.noreply.github.com>
2022-12-24 11:05:08 +0500

    update chayGateway and chatInteractor, add Scene

--- UnityPart/AlisahonVA/Assets/Scripts/Interactors/ChatInteractor.cs
+++ UnityPart/AlisahonVA/Assets/Scripts/Interactors/ChatInteractor.cs
@@ -1,9 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ChatInteractor
 {
-   
+    public static ChatInteractor Instance;
+    public UnityEvent<DialogUIResponseModel> GetResponse;
+
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    public void Initialize()
+    {
+        Instance = this;
+        GetResponse.AddListener(SetData);
+    }
+
+    public void SendAudio(byte[] audioClip)
+    {
+        ChatGateway.Instance.PostAudio(audioClip);
+    }
+
+    public void SendText(string text)
+    {
+        ChatGateway.Instance.PostText(text);
+    }
+
+    public void SetData(DialogUIResponseModel uIResponseModel)
+    {
+       
+    }
 
 }
